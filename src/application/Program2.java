4e008a19eecc4207c35793fbdbdfa49de6b4fdc7
Riann Silva <ooriannsilva@gmail.com>
2023-02-24 21:29:00 -0300

    DepartmentDaoJDBC insert implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,21 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();		
+		
+		System.out.println("Test #1: Department insert ");
+		Department department = new Department(null, "Music");
+		departmentDao.insert(department);
+		System.out.println("Inserted! New Id: " + department.getId());
+
+
+	}
+
+}
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,35 +1,64 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
-	
+public class DepartmentDaoJDBC implements DepartmentDao {
+
 	private Connection conn;
-	
+
 	public DepartmentDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
 
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement pst = null;
+		try {
+			pst = conn.prepareStatement("INSERT INTO department (Name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+
+			pst.setString(1, obj.getName());
+
+			int rowsAffected = pst.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = pst.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+			}
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.CloseStatement(pst);
+		}
 	}
 
 	@Override
 	public void update(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
